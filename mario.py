12d9d95f15dcc223d5d00e171d1e73f8aa965f96
hooha96 <hooha96@users.noreply.github.com>
2020-10-20 06:52:13 +0000

    automated commit by submit50

--- mario.py
+++ mario.py
@@ -1,14 +1,13 @@
 from cs50 import get_int
 
+# checks to see if input is valid, i.e between 1 and 8 inclusive
 while True:
     height = get_int("Height: ")
     if height >= 1 and height <= 8:
         break
 
+# prints the right-aligned half pyramid
 j = height-1
 for i in range(1, height+1, 1):
     print((j * " ") + (i * "#"))
     j -= 1
-
-#print("")
-
